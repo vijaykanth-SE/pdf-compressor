0354854238debf84a03874c5c5baff2adbe535fc
vijay kanth <vijaykanth@perfectgroupus.com>
2026-05-19 13:22:13 -0500

    BE - 03 done wired ghostscript to compress one file

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File, HTTPException
+from fastapi.responses import FileResponse
+import shutil
+import os
+from pathlib import Path
+import subprocess
+
 
 app = FastAPI()
+
+#folder to temp hold upload files
+UPLOAD_DIR = Path("uploads")
+UPLOAD_DIR.mkdir(exist_ok=True)
+COMPRESSED_DIR = Path("compressed")
+COMPRESSED_DIR.mkdir(exist_ok=True)
+
+GHOSTSCRIPT_PATH = r"C:\\Program Files\\gs\\gs10.07.1\\bin\\gswin64c.exe"
+
 
 @app.get("/")
 def root():
@@ -14,3 +29,43 @@ def health():
         "service": "pdf-compressor",
         "version": "0.1.0",
     }
+
+def compress_pdf(input_path: Path,output_path: Path)-> None:
+    #run the ghostscript to compress a pdf
+    result = subprocess.run(
+        [ GHOSTSCRIPT_PATH,
+            "-sDEVICE=pdfwrite",
+            "-dCompatibilityLevel=1.4",
+            "-dPDFSETTINGS=/screen",
+            "-dNOPAUSE",
+            "-dQUIET",
+            "-dBATCH",
+            f"-sOutputFile={output_path}",
+            str(input_path),],
+        capture_output=True,
+        text=True,
+    )
+    if result.returncode != 0:
+        raise RuntimeError(f"Ghostscript failed: {result.stderr}")
+    
+@app.post("/api/compress")
+async def compress(file: UploadFile = File(...)):
+    if not file.filename.lower().endswith(".pdf"):
+        raise HTTPException(status_code=400, detail="File must be a pdf")
+    
+    #save uploaded file to the disk
+    input_path = UPLOAD_DIR/file.filename
+    with open(input_path, "wb") as f:
+        shutil.copyfileobj(file.file, f)
+
+    output_path = COMPRESSED_DIR / f"compressed_{file.filename}"
+    try:
+        compress_pdf(input_path, output_path)
+    except RuntimeError as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+    return FileResponse(
+        path=output_path,
+        media_type="application/pdf",
+        filename=f"compressed_{file.filename}",
+    )
